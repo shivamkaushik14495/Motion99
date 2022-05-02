6f7410c86e8863097f98315479a51a3ccc93fa80
shivamkaushik14495 <44652998+shivamkaushik14495@users.noreply.github.com>
2022-05-02 16:04:58 +0530

    Random Password Files

---
+++ script.js
@@ -0,0 +1,18 @@
+const arr = [1,2,3,4,5,6,7,8,9,0,"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+let value = document.querySelector(".password");
+const btn = document.getElementById("Password_generate");
+
+//let password = ""
+btn.addEventListener("click" , function() {
+    let securePassword = "";
+    let length = 6;
+    for(let i=0 ; i<length ; i++){
+        securePassword += arr[randomNumber()];
+    }
+    value.textContent = securePassword;
+})
+
+function randomNumber(){
+    return Math.floor(Math.random()*arr.length);
+};
